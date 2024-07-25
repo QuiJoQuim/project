c6a19352597307b78f34f5227faad2d34b6c2b6c
ntsirintanis <ntsirintanis@therp.nl>
2024-07-25 14:20:22 +0200

    [ADD] project_task_creation_description_notification

---
+++ project_task_creation_description_notification/models/project_task.py
@@ -0,0 +1,25 @@
+# © 2022 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class ProjectTask(models.Model):
+    """Task description in notification"""
+
+    _inherit = "project.task"
+
+    @api.returns("mail.message", lambda value: value.id)
+    def message_post(self, *args, body="", subtype_id=False, **kwargs):
+        create_subtype = self._creation_subtype()
+        if not body and subtype_id == (create_subtype and create_subtype.id):
+            body = "".join(
+                [
+                    create_subtype.name,
+                    "<br/>",
+                    ("Task Description: <br/> %s <br/>" % self.description)
+                    if self.description
+                    else "",
+                ]
+            )
+        return super().message_post(*args, body=body, subtype_id=subtype_id, **kwargs)
